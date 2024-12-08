642827f8e54e0e756e779afde72cdd8d6ee431a3
Mohammed Mohammed <142446903+Mohammed3ohammed@users.noreply.github.com>
2024-12-08 21:35:16 +0200

    Add Review component with order summary functionality

--- src/App.js
+++ src/App.js
@@ -6,60 +6,85 @@ import Product from './components/Products/Product/Product';
 import Cart from './components/Cart/Cart';
 import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
 import Checkout from './components/CheckoutForm/Check/Checkout.jsx';
+import { CssBaseline } from '@material-ui/core';
 
 
 const App = () => {
+    const [mobileOpen, setMobileOpen] = React.useState(false);
     const [products, setProducts] = useState([]); 
-    const [cart, setCart] = useState({})
+    const [cart, setCart] = useState({});
+    const [order, setOrder] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
+
     const fetchProducts = async () => {
       const { data } = await commerce.products.list();
 
       setProducts(data)
-    } 
+    } ;
 
     const fetchCart = async () => {
       
-      setCart( await commerce.cart.retrieve())
-    }
+      setCart( await commerce.cart.retrieve());
+    };
 
     const handleAddToCart = async (productId, quantity) => {
-      const { cart } = await commerce.cart.add(productId, quantity);
+      const item = await commerce.cart.add(productId, quantity);
 
-      setCart(cart);
-    }
+      setCart(item.cart);
+    };
 
-    const handleUpdateCartOty = async (productId, quantity) => {
-      const response  = await commerce.cart.add(productId, quantity);
+    const handleUpdateCartOty = async (lineItemId, quantity) => {
+      const response  = await commerce.cart.update(lineItemId, { quantity });
 
       setCart(response.cart);
-    }
+    };
 
-    const handleRemoveFromCart = async (productId) => {
-      const { cart }  = await commerce.cart.remove(productId);
-      setCart(cart);
-    }
+    const handleRemoveFromCart = async (lineItemId) => {
+     const response = await commerce.cart.remove(lineItemId);
+      setCart(response.cart);
+    };
 
     const handleEmptyCart = async () => {
-      const { cart } = await commerce.cart.empty();
+      const response = await commerce.cart.empty();
 
-      setCart(cart);
-    }
+      setCart(response.cart);
+    };
 
+    const refreshCart = async () => {
+      const newCart = await commerce.cart.refresh();
 
+      setCart(newCart);
+    };
+
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+      try {
+        const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+        setOrder(incomingOrder);
+
+        refreshCart();
+
+      } catch (error) {
+        setErrorMessage(error.data.error.message);
+      }
+    };
+ 
     useEffect(() => {
       fetchProducts();
       fetchCart();
     }, []);
 
-    console.log(cart)
+   const handleDrawerToggle = () => setMobileOpen(!mobileOpen); 
+
   return (
 
     <Router>
-      <div>
-     <Navbar totalItems={cart.total_items} />
+      <div style={{display: 'flex'}}>
+        <CssBaseline />
+     <Navbar totalItems={cart.total_items}  handleDrawerToggle={handleDrawerToggle}/>
      <Switch>
           <Route exact path="/">
-          <Product products={products} onAddToCart={handleAddToCart} />
+          <Product products={products} onAddToCart={handleAddToCart} handleUpdateCartOty />
           </Route>
           <Route exact path='/cart'>
           <Cart 
@@ -71,7 +96,7 @@ const App = () => {
           />
           </Route>
           <Route exact path="/checkout">
-            <Checkout />
+            <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
           </Route>
      </Switch>
     </div>
--- src/components/CheckoutForm/Check/Checkout.jsx
+++ src/components/CheckoutForm/Check/Checkout.jsx
@@ -1,15 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import {CssBaseline, Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core';
-import Commerce from '@chec/commerce.js';
-
-
-
+import useStyles from './styles';
+import { Link, useHistory } from 'react-router-dom';
+import { commerce } from '../../../lib/commerce';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 const steps = ['Shipping address', 'Payment details'];
 
 const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
   const [checkoutToken, setCheckoutToken ] = useState(null);
-  const [activeStep, setActiveStep] = useState(null);
     const [activeStep, setActiveStep] = useState(0);
     const [shippingData, setShippingData] = useState({});
     const classes = useStyles();
@@ -22,14 +22,14 @@ const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
         if(cart.id) {
           const generateToken = async () => {
             try {
-              const token = await Commerce.checkout.generateToken(cart.id, {type: 'cart'});
+              const token = await commerce.checkout.generateToken(cart.id, {type: 'cart'});
               setCheckoutToken(token)
             } catch {
               if (activeStep !== steps.length) history.push('/');
             }
-          }
+          };
           generateToken();
-        };
+        }
 
         
       }, [cart]);
@@ -45,10 +45,10 @@ const Checkout = ({ cart, onCaptureCheckout, order, error }) => {
         <div>
           <Typography variant="h5"> Thank you for your purchase,{order.customer.firstname}  {order.customer.lastname}! </Typography>
           <Divider className={classes.divider} />
-          <Typography variant='subtitle2'>Order ref: {order.Commerce__reference}</Typography>
+          <Typography variant='subtitle2'>Order ref: {order.customer_reference}</Typography>
           </div>
             <br />
-            <Button onCompositionEndCapture={Link} variant='outlined' type="button" to="/"> Back to home</Button>
+            <Button component={Link} variant='outlined' type="button" to="/"> Back to home</Button>
           </>
       ) : (
         <div className={classes.spinner}>
